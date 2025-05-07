5acc3f16b8fd305854746ee6f4052e7310485970
dmitri minckevich <rauckees0811@gmail.com>
2025-05-07 15:43:38 +0300

    feat: add basic app module with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CardsModule } from './cards/cards.module';
+import { ColumnsModule } from './columns/columns.module';
+import { CommentsModule } from './comments/comments.module';
+import { CardOwnerGuard } from './common/guards/card-owner.guard';
+import { ColumnOwnerGuard } from './common/guards/column-owner.guard';
+import { CommentOwnerGuard } from './common/guards/comment-owner.guard';
 
 @Module({
-  imports: [],
+  imports: [CardsModule, ColumnsModule, CommentsModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CardOwnerGuard, ColumnOwnerGuard, CommentOwnerGuard],
 })
 export class AppModule {}
